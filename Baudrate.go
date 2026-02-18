275c5ffcce10e4385e1cfca0d329c46686548234
Gurux Ltd <gurux@gurux.fi>
2026-02-18 10:19:46 +0200

    Version 1.0.9
    fix: update baud rate enum naming

--- Baudrate.go
+++ Baudrate.go
@@ -63,46 +63,46 @@ import (
 type BaudRate int
 
 const (
-	// Baud50 defines that the baudrate is 50.
-	Baud50 BaudRate = 50
-	// Baud75 defines that the baudrate is 75.
-	Baud75 BaudRate = 75
-	// Baud110 defines that the baudrate is 110.
-	Baud110 BaudRate = 110
-	// Baud134 defines that the baudrate is 134.
-	Baud134 BaudRate = 134
-	// Baud150 defines that the baudrate is 150.
-	Baud150 BaudRate = 150
-	// Baud200 defines that the baudrate is 200.
-	Baud200 BaudRate = 200
-	// Baud300 defines that the baudrate is 300.
-	Baud300 BaudRate = 300
-	// Baud600 defines that the baudrate is 600.
-	Baud600 BaudRate = 600
-	// Baud1200 defines that the baudrate is 1200.
-	Baud1200 BaudRate = 1200
-	// Baud1800 defines that the baudrate is 1800.
-	Baud1800 BaudRate = 1800
-	// Baud2400 defines that the baudrate is 2400.
-	Baud2400 BaudRate = 2400
-	// Baud4800 defines that the baudrate is 4800.
-	Baud4800 BaudRate = 4800
-	// Baud9600 defines that the baudrate is 9600.
-	Baud9600 BaudRate = 9600
-	// Baud19200 defines that the baudrate is 19200.
-	Baud19200 BaudRate = 19200
-	// Baud38400 defines that the baudrate is 38400.
-	Baud38400 BaudRate = 38400
-	// Baud57600 defines that the baudrate is 57600.
-	Baud57600 BaudRate = 57600
-	// Baud115200 defines that the baudrate is 115200.
-	Baud115200 BaudRate = 115200
-	// Baud230400 defines that the baudrate is 230400.
-	Baud230400 BaudRate = 230400
-	// Baud460800 defines that the baudrate is 460800.
-	Baud460800 BaudRate = 460800
-	// Baud921600 defines that the baudrate is 921600.
-	Baud921600 BaudRate = 921600
+	// BaudRate50 defines that the baudrate is 50.
+	BaudRate50 BaudRate = 50
+	// BaudRate75 defines that the baudrate is 75.
+	BaudRate75 BaudRate = 75
+	// BaudRate110 defines that the baudrate is 110.
+	BaudRate110 BaudRate = 110
+	// BaudRate134 defines that the baudrate is 134.
+	BaudRate134 BaudRate = 134
+	// BaudRate150 defines that the baudrate is 150.
+	BaudRate150 BaudRate = 150
+	// BaudRate200 defines that the baudrate is 200.
+	BaudRate200 BaudRate = 200
+	// BaudRate300 defines that the baudrate is 300.
+	BaudRate300 BaudRate = 300
+	// BaudRate600 defines that the baudrate is 600.
+	BaudRate600 BaudRate = 600
+	// BaudRate1200 defines that the baudrate is 1200.
+	BaudRate1200 BaudRate = 1200
+	// BaudRate1800 defines that the baudrate is 1800.
+	BaudRate1800 BaudRate = 1800
+	// BaudRate2400 defines that the baudrate is 2400.
+	BaudRate2400 BaudRate = 2400
+	// BaudRate4800 defines that the baudrate is 4800.
+	BaudRate4800 BaudRate = 4800
+	// BaudRate9600 defines that the baudrate is 9600.
+	BaudRate9600 BaudRate = 9600
+	// BaudRate19200 defines that the baudrate is 19200.
+	BaudRate19200 BaudRate = 19200
+	// BaudRate38400 defines that the baudrate is 38400.
+	BaudRate38400 BaudRate = 38400
+	// BaudRate57600 defines that the baudrate is 57600.
+	BaudRate57600 BaudRate = 57600
+	// BaudRate115200 defines that the baudrate is 115200.
+	BaudRate115200 BaudRate = 115200
+	// BaudRate230400 defines that the baudrate is 230400.
+	BaudRate230400 BaudRate = 230400
+	// BaudRate460800 defines that the baudrate is 460800.
+	BaudRate460800 BaudRate = 460800
+	// BaudRate921600 defines that the baudrate is 921600.
+	BaudRate921600 BaudRate = 921600
 )
 
 // BaudRateParse converts the given string into a Baudrate value.
@@ -114,45 +114,45 @@ func BaudRateParse(value string) (BaudRate, error) {
 	var err error
 	switch {
 	case strings.EqualFold(value, "50"):
-		ret = Baud50
+		ret = BaudRate50
 	case strings.EqualFold(value, "75"):
-		ret = Baud75
+		ret = BaudRate75
 	case strings.EqualFold(value, "110"):
-		ret = Baud110
+		ret = BaudRate110
 	case strings.EqualFold(value, "134"):
-		ret = Baud134
+		ret = BaudRate134
 	case strings.EqualFold(value, "150"):
-		ret = Baud150
+		ret = BaudRate150
 	case strings.EqualFold(value, "200"):
-		ret = Baud200
+		ret = BaudRate200
 	case strings.EqualFold(value, "300"):
-		ret = Baud300
+		ret = BaudRate300
 	case strings.EqualFold(value, "600"):
-		ret = Baud600
+		ret = BaudRate600
 	case strings.EqualFold(value, "1200"):
-		ret = Baud1200
+		ret = BaudRate1200
 	case strings.EqualFold(value, "1800"):
-		ret = Baud1800
+		ret = BaudRate1800
 	case strings.EqualFold(value, "2400"):
-		ret = Baud2400
+		ret = BaudRate2400
 	case strings.EqualFold(value, "4800"):
-		ret = Baud4800
+		ret = BaudRate4800
 	case strings.EqualFold(value, "9600"):
-		ret = Baud9600
+		ret = BaudRate9600
 	case strings.EqualFold(value, "19200"):
-		ret = Baud19200
+		ret = BaudRate19200
 	case strings.EqualFold(value, "38400"):
-		ret = Baud38400
+		ret = BaudRate38400
 	case strings.EqualFold(value, "57600"):
-		ret = Baud57600
+		ret = BaudRate57600
 	case strings.EqualFold(value, "115200"):
-		ret = Baud115200
+		ret = BaudRate115200
 	case strings.EqualFold(value, "230400"):
-		ret = Baud230400
+		ret = BaudRate230400
 	case strings.EqualFold(value, "460800"):
-		ret = Baud460800
+		ret = BaudRate460800
 	case strings.EqualFold(value, "921600"):
-		ret = Baud921600
+		ret = BaudRate921600
 	default:
 		err = fmt.Errorf("%w: %q", ErrUnknownEnum, value)
 	}
@@ -164,45 +164,45 @@ func BaudRateParse(value string) (BaudRate, error) {
 func (g BaudRate) String() string {
 	var ret string
 	switch g {
-	case Baud50:
+	case BaudRate50:
 		ret = "50"
-	case Baud75:
+	case BaudRate75:
 		ret = "75"
-	case Baud110:
+	case BaudRate110:
 		ret = "110"
-	case Baud134:
+	case BaudRate134:
 		ret = "134"
-	case Baud150:
+	case BaudRate150:
 		ret = "150"
-	case Baud200:
+	case BaudRate200:
 		ret = "200"
-	case Baud300:
+	case BaudRate300:
 		ret = "300"
-	case Baud600:
+	case BaudRate600:
 		ret = "600"
-	case Baud1200:
+	case BaudRate1200:
 		ret = "1200"
-	case Baud1800:
+	case BaudRate1800:
 		ret = "1800"
-	case Baud2400:
+	case BaudRate2400:
 		ret = "2400"
-	case Baud4800:
+	case BaudRate4800:
 		ret = "4800"
-	case Baud9600:
+	case BaudRate9600:
 		ret = "9600"
-	case Baud19200:
+	case BaudRate19200:
 		ret = "19200"
-	case Baud38400:
+	case BaudRate38400:
 		ret = "38400"
-	case Baud57600:
+	case BaudRate57600:
 		ret = "57600"
-	case Baud115200:
+	case BaudRate115200:
 		ret = "115200"
-	case Baud230400:
+	case BaudRate230400:
 		ret = "230400"
-	case Baud460800:
+	case BaudRate460800:
 		ret = "460800"
-	case Baud921600:
+	case BaudRate921600:
 		ret = "921600"
 	}
 	return ret
@@ -211,25 +211,25 @@ func (g BaudRate) String() string {
 // AllBaudrate returns a slice containing all defined baudrate values.
 func AllBaudrate() []BaudRate {
 	return []BaudRate{
-		Baud50,
-		Baud75,
-		Baud110,
-		Baud134,
-		Baud150,
-		Baud200,
-		Baud300,
-		Baud600,
-		Baud1200,
-		Baud1800,
-		Baud2400,
-		Baud4800,
-		Baud9600,
-		Baud19200,
-		Baud38400,
-		Baud57600,
-		Baud115200,
-		Baud230400,
-		Baud460800,
-		Baud921600,
+		BaudRate50,
+		BaudRate75,
+		BaudRate110,
+		BaudRate134,
+		BaudRate150,
+		BaudRate200,
+		BaudRate300,
+		BaudRate600,
+		BaudRate1200,
+		BaudRate1800,
+		BaudRate2400,
+		BaudRate4800,
+		BaudRate9600,
+		BaudRate19200,
+		BaudRate38400,
+		BaudRate57600,
+		BaudRate115200,
+		BaudRate230400,
+		BaudRate460800,
+		BaudRate921600,
 	}
 }
